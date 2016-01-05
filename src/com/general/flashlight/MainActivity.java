c619624b83b399a6e6d6f94258582d368cf68eb0
wpj <wpj0304@163.com>
2016-01-05 21:49:03 +0800

    添加轮训检查广告功能

--- src/com/general/flashlight/MainActivity.java
+++ src/com/general/flashlight/MainActivity.java
@@ -1,16 +1,22 @@
 package com.general.flashlight;
 
 
+
 import com.general.waps.Advertisement;
 import com.general.waps.QuitPopAd;
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.onlineconfig.OnlineConfigAgent;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
@@ -21,19 +27,21 @@ public class MainActivity extends Activity implements View.OnClickListener
 {
 
 	private LinearLayout lLayout;
-	int NumBut01 = 0;
+	private int NumBut01 = 0;
 	private Button but01;
-	WindowManager.LayoutParams layoutParams;
-	Camera camera;
-	Parameters parameter;
-	boolean advState = false;
+	private WindowManager.LayoutParams layoutParams;
+	private Camera camera;
+	private Parameters parameter;
+	private boolean advState = false;
+	private int checkCount = 0;// 检查广告是否开启次数
+	private final int CC_AD_MAXC = 3; // 检查次数
+	private Handler handler;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
 		camera = Camera.open();
 		parameter = camera.getParameters();
 
@@ -42,12 +50,18 @@ public class MainActivity extends Activity implements View.OnClickListener
 		but01 = (Button) findViewById(R.id.but01);
 
 		but01.setOnClickListener(this);
+		Looper looper = Looper.myLooper();
+		handler = new MessageHandler(looper);
+		showAd();
 		
+	}
+	
+	private void showAd()
+	{
 		// 友盟在线参数
 		OnlineConfigAgent.getInstance().updateOnlineConfig(this);
 		String showAdc = OnlineConfigAgent.getInstance().getConfigParams(this, "show_adv"); // 是否显示广告
 		// 广告 ------------
-		
 		if("0".equals(showAdc))
 		{
 			advState = true;
@@ -63,8 +77,26 @@ public class MainActivity extends Activity implements View.OnClickListener
 			}
 			Advertisement.getInstance(this).showAdSelectad(true, floatState, true);
 		}
-		
-		
+		if(!advState && checkCount < CC_AD_MAXC)
+		{
+			// 广告未加载再次检查
+			checkCount++;
+			
+			new Thread(new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					try
+					{
+						Thread.sleep(2000);
+					} catch (Exception e)
+					{}
+					 Message message = Message.obtain();
+					 handler.sendMessage(message);
+				}
+			}).start();
+		}
 	}
 
 	@Override
@@ -154,5 +186,17 @@ public class MainActivity extends Activity implements View.OnClickListener
 		camera.release();
 		Advertisement.getInstance(this).close();
 	}
+	
+	 //子类化一个Handler
+    class MessageHandler extends Handler {
+        public MessageHandler(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+        	showAd();
+        }
+    }
 
 }
